d0a46cb67736ae03fdb1154386f2448dd43e5239
shreevidyamatabhuvanesvari-gif <shreevidyamatabhuvanesvari@gmail.com>
2026-06-14 01:00:37 +0530

    Create command-bootstrap-v3.js

---
+++ assets/js/command-bootstrap-v3.js
@@ -0,0 +1,92 @@
+(() => {
+
+    function registerInsightCommands() {
+
+        CommandRegistry.register(
+
+            "insight",
+
+            () => ({
+
+                success: true,
+
+                report:
+                    InsightEngine
+                    .generateAllInsights()
+            })
+        );
+
+        CommandRegistry.register(
+
+            "insightsummary",
+
+            () => ({
+
+                success: true,
+
+                summary:
+                    InsightEngine
+                    .summary()
+            })
+        );
+
+        CommandRegistry.register(
+
+            "learninginsights",
+
+            () => ({
+
+                success: true,
+
+                learning:
+                    InsightEngine
+                    .generateLearningRecommendations()
+            })
+        );
+
+        CommandRegistry.register(
+
+            "memoryinsights",
+
+            () => ({
+
+                success: true,
+
+                memory:
+                    InsightEngine
+                    .generateMemoryInsights()
+            })
+        );
+
+        CommandRegistry.register(
+
+            "sharedconcepts",
+
+            () => ({
+
+                success: true,
+
+                concepts:
+                    InsightEngine
+                    .generateSharedConceptInsights()
+            })
+        );
+
+        CommandRegistry.register(
+
+            "clusters",
+
+            () => ({
+
+                success: true,
+
+                clusters:
+                    InsightEngine
+                    .generateClusterInsights()
+            })
+        );
+    }
+
+    registerInsightCommands();
+
+})();
